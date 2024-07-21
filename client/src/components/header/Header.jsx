0eeed87f307e7943eb126e5a9229ccb790ead15f
BorislavRaynov <b_raynov@abv.bg>
2024-07-21 18:37:41 +0300

    Links added.

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,14 +1,16 @@
+import { Link } from 'react-router-dom'
+
 import { useState } from 'react'
 
-import { Dialog, DialogPanel } from '@headlessui/react'
+// import { Dialog, DialogPanel } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 
 
 const navigation = [
-    { name: 'CarList', href: '#' },
-    { name: 'Create Ad', href: '#' },
-    { name: 'Contacts', href: '#' },
-    { name: 'About', href: '#' },
+    { name: 'CarList', to: '/cars' },
+    { name: 'Create Ad', to: '/cars/create' },
+    { name: 'Contacts', to: '/contacts' },
+    { name: 'About', to: '/about' },
 ]
 
 export default function Header() {
@@ -18,14 +20,14 @@ export default function Header() {
         <header className="absolute inset-x-0 top-0 z-50">
             <nav aria-label="Global" className="flex items-center justify-between p-6 lg:px-8">
                     <div className="flex lg:flex-1">
-                        <a href="#" className="-m-1.5 p-1.5">
-                            <span className="sr-only">Your Company</span>
+                        <Link to="/" className="-m-1.5 p-1.5">
+                            <span className="sr-only">Home</span>
                             <img
                                 alt=""
-                                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                                src="https://static.thenounproject.com/png/3574480-200.png"
                                 className="h-8 w-auto"
                             />
-                        </a>
+                        </Link>
                     </div>
                     <div className="flex lg:hidden">
                         <button
@@ -39,35 +41,35 @@ export default function Header() {
                     </div>
                     <div className="hidden lg:flex lg:gap-x-12">
                         {navigation.map((item) => (
-                            <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-gray-900">
+                            <Link key={item.name} to={item.to} className="text-sm font-semibold leading-6 text-gray-900">
                                 {item.name}
-                            </a>
+                            </Link>
                         ))}
                     </div>
                     <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-                        <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+                        <Link to="/logout" className="text-sm font-semibold leading-6 text-gray-900">
                         Log out <span aria-hidden="true">&rarr;</span>
-                        </a>
-                        <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+                        </Link>
+                        <Link to="/login" className="text-sm font-semibold leading-6 text-gray-900">
                         Log in <span aria-hidden="true">&rarr;</span>
-                        </a>
-                        <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+                        </Link>
+                        <Link to="/register" className="text-sm font-semibold leading-6 text-gray-900">
                         Register <span aria-hidden="true">&rarr;</span>
-                        </a>
+                        </Link>
                     </div>
             </nav>
-            <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
+            {/* <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
             <div className="fixed inset-0 z-50" />
                 <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                     <div className="flex items-center justify-between">
-                        <a href="#" className="-m-1.5 p-1.5">
-                            <span className="sr-only">Your Company</span>
+                        <Link to="#" className="-m-1.5 p-1.5">
+                            <span className="sr-only">Home</span>
                             <img
                             alt=""
-                            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                            src="https://static.thenounproject.com/png/3574480-200.png"
                             className="h-8 w-auto"
                             />
-                        </a>
+                        </Link>
                         <button
                             type="button"
                             onClick={() => setMobileMenuOpen(false)}
@@ -81,27 +83,27 @@ export default function Header() {
                         <div className="-my-6 divide-y divide-gray-500/10">
                             <div className="space-y-2 py-6">
                                 {navigation.map((item) => (
-                                    <a
+                                    <Link
                                     key={item.name}
-                                    href={item.href}
+                                    to={item.to}
                                     className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                                     >
                                     {item.name}
-                                    </a>
+                                    </Link>
                                 ))}
                             </div>
                             <div className="py-6">
-                                <a
-                                    href="#"
+                                <Link
+                                    to="#"
                                     className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                                 >
                                     Log in
-                                </a>
+                                </Link>
                             </div>
                         </div>
                     </div>
                 </DialogPanel>
-            </Dialog>
+            </Dialog> */}
         </header>
     );
 }
