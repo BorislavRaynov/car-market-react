e4407801c8b240494e548e2ae405a54364b26bb9
BorislavRaynov <b_raynov@abv.bg>
2024-07-27 09:42:57 +0300

    FormControl added.

--- client/src/components/car-ad-create/CarAdCreate.jsx
+++ client/src/components/car-ad-create/CarAdCreate.jsx
@@ -1,11 +1,40 @@
-import { PhotoIcon, UserCircleIcon } from '@heroicons/react/24/solid'
+// import { PhotoIcon, UserCircleIcon } from '@heroicons/react/24/solid'
+
+import { useEffect, useRef, useState } from "react"
+
+const initialValues = {
+	make: '',
+	model: '',
+	mileage: '',
+	fuel: '',
+	img1: '',
+	img2: '',
+	img3: '',
+	img4: '',
+	info: '',
+}
 
 export default function CarAdCreate() {
+	const [carData, setCarData] = useState(initialValues)
 
+	const formSubmitHandler = (e) => {
+		e.preventDefault();
+		console.log('Form Submitted!!!');
+	}
+
+	const changeHandler = (e) => {
+		setCarData(carData => ({...carData, [e.target.name]: e.target.value}))
+	}
+
+	const inputRef = useRef();
+
+	useEffect(() => {
+		inputRef.current.focus()
+	},[])
 
 	return (
 		<div className='mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-4xl lg:px-8'>
-			<form>
+			<form onSubmit={formSubmitHandler}>
 				<div className="space-y-12">
 					<div className="border-b border-gray-900/10 pb-12">
 						<h2 className="text-base font-semibold leading-7 text-gray-900">Enter vehicle details</h2>
@@ -18,11 +47,14 @@ export default function CarAdCreate() {
 								<div className="mt-2">
 									<div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
 										<input
+											className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
 											id="make"
 											name="make"
 											type="text"
+											ref={inputRef}
 											placeholder="Mercedes"
-											className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+											value={carData.make}
+											onChange={changeHandler}
 										/>
 									</div>
 								</div>
@@ -35,11 +67,13 @@ export default function CarAdCreate() {
 								<div className="mt-2">
 									<div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
 										<input
+											className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
 											id="model"
 											name="model"
 											type="text"
 											placeholder="G500 AMG"
-											className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+											value={carData.model}
+											onChange={changeHandler}
 										/>
 									</div>
 								</div>
@@ -52,11 +86,13 @@ export default function CarAdCreate() {
 								<div className="mt-2">
 									<div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
 										<input
+											className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
 											id="mileage"
 											name="mileage"
 											type="text"
 											placeholder="150000"
-											className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+											value={carData.mileage}
+											onChange={changeHandler}
 										/>
 									</div>
 								</div>
@@ -69,11 +105,13 @@ export default function CarAdCreate() {
 								<div className="mt-2">
 									<div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
 										<input
+											className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
 											id="fuel"
 											name="fuel"
 											type="text"
 											placeholder="Petrol"
-											className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+											value={carData.fuel}
+											onChange={changeHandler}
 										/>
 									</div>
 								</div>
@@ -90,10 +128,12 @@ export default function CarAdCreate() {
 								<div className="mt-2">
 									<div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
 										<input
+											className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
 											id="img1"
 											name="img1"
 											type="text"
-											className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+											value={carData.img1}
+											onChange={changeHandler}
 										/>
 									</div>
 								</div>
@@ -106,10 +146,12 @@ export default function CarAdCreate() {
 								<div className="mt-2">
 									<div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
 										<input
+											className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
 											id="img2"
 											name="img2"
 											type="text"
-											className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+											value={carData.img2}
+											onChange={changeHandler}
 										/>
 									</div>
 								</div>
@@ -122,10 +164,12 @@ export default function CarAdCreate() {
 								<div className="mt-2">
 									<div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
 										<input
+											className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
 											id="img3"
 											name="img3"
 											type="text"
-											className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+											value={carData.img3}
+											onChange={changeHandler}
 										/>
 									</div>
 								</div>
@@ -138,10 +182,12 @@ export default function CarAdCreate() {
 								<div className="mt-2">
 									<div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
 										<input
+											className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
 											id="img4"
 											name="img4"
 											type="text"
-											className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+											value={carData.img4}
+											onChange={changeHandler}
 										/>
 									</div>
 								</div>
@@ -155,11 +201,12 @@ export default function CarAdCreate() {
 							</label>
 							<div className="mt-2">
 								<textarea
+									className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
 									id="info"
 									name="info"
 									rows={3}
-									className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-									defaultValue={''}
+									value={carData.info}
+									onChange={changeHandler}
 								/>
 							</div>
 						</div>
@@ -171,8 +218,9 @@ export default function CarAdCreate() {
 						Cancel
 					</button>
 					<button
-						type="submit"
 						className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+						type="submit"
+						onClick={formSubmitHandler}
 					>
 						Save
 					</button>
