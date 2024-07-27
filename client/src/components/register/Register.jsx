5a07861c688b28fb01cc2406e3b98945a9748313
BorislavRaynov <b_raynov@abv.bg>
2024-07-27 11:15:28 +0300

    Register formcontrol added.

--- client/src/components/register/Register.jsx
+++ client/src/components/register/Register.jsx
@@ -1,4 +1,30 @@
+import { useEffect, useRef, useState } from "react"
+
+const initialUserFormValues = {
+	email: '',
+	password: '',
+}
+
 export default function Register() {
+    const [userFormValues, setUserFormValues] = useState(initialUserFormValues)
+
+    const formUserSubmitHandler = (e) => {
+        e.preventDefault()
+        console.log("User register form submitted")
+        console.log(userFormValues)
+    }
+
+    const changeHandler = (e) => {
+        setUserFormValues(userFormValues => ({...userFormValues, [e.target.name]: e.target.value}))
+    }
+
+    
+	const inputRef = useRef();
+
+	useEffect(() => {
+		inputRef.current.focus()
+	},[])
+
     return (
         <>
             <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-24 lg:px-8">
@@ -9,19 +35,22 @@ export default function Register() {
                 </div>
 
                 <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                    <form action="#" method="POST" className="space-y-6">
+                    <form className="space-y-6" onSubmit={formUserSubmitHandler}>
                         <div>
                             <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
                                 Email address
                             </label>
                             <div className="mt-2">
                                 <input
+                                    required
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                     id="email"
                                     name="email"
                                     type="email"
-                                    required
                                     autoComplete="email"
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    ref={inputRef}
+                                    value={userFormValues.email}
+                                    onChange={changeHandler}
                                 />
                             </div>
                         </div>
@@ -34,20 +63,23 @@ export default function Register() {
                             </div>
                             <div className="mt-2">
                                 <input
+                                    required
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                     id="password"
                                     name="password"
                                     type="password"
-                                    required
                                     autoComplete="current-password"
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    value={userFormValues.password}
+                                    onChange={changeHandler}
                                 />
                             </div>
                         </div>
 
                         <div>
                             <button
-                                type="submit"
                                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                type="submit"
+                                onClick={formUserSubmitHandler}
                             >
                                 Register
                             </button>
