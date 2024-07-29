408c2df42573b3bb7223cd65de2e2e40af6ce6c7
BorislavRaynov <b_raynov@abv.bg>
2024-07-29 16:02:41 +0300

    Auth state added.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,42 @@
-import { Routes, Route } from 'react-router-dom'
+import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
 
-import CarAdCreate from "./components/car-ad-create/CarAdCreate.jsx"
-import CarDetails from "./components/car-details/CarDetails.jsx"
-import CarList from "./components/car-list/CarList.jsx"
-import Header from "./components/header/Header.jsx"
-import Home from "./components/home/Home.jsx"
-import Login from "./components/login/Login.jsx"
-import Logout from "./components/logout/Logout.jsx"
-import Register from "./components/register/Register.jsx"
-import About from './components/about/About.jsx'
-import Favorites from './components/favorites/Favorites.jsx'
+import { AuthContext } from './contexts/AuthContext.js';
+
+import CarAdCreate from "./components/car-ad-create/CarAdCreate.jsx";
+import CarDetails from "./components/car-details/CarDetails.jsx";
+import CarList from "./components/car-list/CarList.jsx";
+import Header from "./components/header/Header.jsx";
+import Home from "./components/home/Home.jsx";
+import Login from "./components/login/Login.jsx";
+import Logout from "./components/logout/Logout.jsx";
+import Register from "./components/register/Register.jsx";
+import About from './components/about/About.jsx';
+import Favorites from './components/favorites/Favorites.jsx';
 
 
 function App() {
+	const [authState, setAuthState] = useState({});
+
+	const changeAuthState = (state) => {
+		// TODO: impl persisted authstate
+		localStorage.setItem('accessToken', state.accessToken)
+		setAuthState(state);
+	}
+		
+
+
+
+	const contextData = {
+		userId: authState._id,
+		email: authState.email,
+		accessToken: authState.accessToken,
+		isAuthenticated: !!authState.email,
+		changeAuthState,
+	};
+
 	return (
-		<>
+		<AuthContext.Provider value={contextData}>
 			<div className="bg-white">
 				<Header />
 
@@ -30,7 +52,7 @@ function App() {
 					<Route path='/about' element={<About />} />
 				</Routes>
 			</div>
-		</>
+		</AuthContext.Provider>
 	)
 }
 
