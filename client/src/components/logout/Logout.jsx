a5b5cc397c4d14124061709db6252f3280e4abf6
BorislavRaynov <b_raynov@abv.bg>
2024-08-01 18:50:20 +0300

    Logout handlers added.

--- client/src/components/logout/Logout.jsx
+++ client/src/components/logout/Logout.jsx
@@ -1,10 +1,26 @@
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 import { Dialog, DialogBackdrop, DialogPanel } from '@headlessui/react'
 import { ExclamationTriangleIcon } from '@heroicons/react/24/outline'
 
+import { useLogout } from '../../hooks/useAuth'
+
 export default function Logout() {
-    const [open, setOpen] = useState(true)
+    const [open, setOpen] = useState(true);
+    const navigate = useNavigate();
+    const logout = useLogout();
+
+    const logoutClickHandler = (e) => {
+        logout();
+        setOpen(false);
+        navigate('/');
+    };
+
+    const cancelClickHandler = (e) => {
+        setOpen(false);
+        navigate('/');
+    }
 
     return (
         <Dialog open={open} onClose={setOpen} className="relative z-10">
@@ -36,7 +52,7 @@ export default function Logout() {
                         <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
                             <button
                                 type="button"
-                                onClick={() => setOpen(false)}
+                                onClick={logoutClickHandler}
                                 className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
                             >
                                 Logout
@@ -44,7 +60,7 @@ export default function Logout() {
                             <button
                                 type="button"
                                 data-autofocus
-                                onClick={() => setOpen(false)}
+                                onClick={cancelClickHandler}
                                 className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
                             >
                                 Cancel
