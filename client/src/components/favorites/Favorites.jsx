f32201d5d5ec1d91250d25d01950f395fa654036
BorislavRaynov <b_raynov@abv.bg>
2024-08-01 18:43:50 +0300

    Favorite component added.

--- client/src/components/favorites/Favorites.jsx
+++ client/src/components/favorites/Favorites.jsx
@@ -1,87 +1,16 @@
-const people = [
-	{
-		name: 'Leslie Alexander',
-		email: 'leslie.alexander@example.com',
-		role: 'Co-Founder / CEO',
-		imageUrl:
-			'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-		lastSeen: '3h ago',
-		lastSeenDateTime: '2023-01-23T13:23Z',
-	},
-	{
-		name: 'Michael Foster',
-		email: 'michael.foster@example.com',
-		role: 'Co-Founder / CTO',
-		imageUrl:
-			'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-		lastSeen: '3h ago',
-		lastSeenDateTime: '2023-01-23T13:23Z',
-	},
-	{
-		name: 'Dries Vincent',
-		email: 'dries.vincent@example.com',
-		role: 'Business Relations',
-		imageUrl:
-			'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-		lastSeen: null,
-	},
-	{
-		name: 'Lindsay Walton',
-		email: 'lindsay.walton@example.com',
-		role: 'Front-end Developer',
-		imageUrl:
-			'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-		lastSeen: '3h ago',
-		lastSeenDateTime: '2023-01-23T13:23Z',
-	},
-	{
-		name: 'Courtney Henry',
-		email: 'courtney.henry@example.com',
-		role: 'Designer',
-		imageUrl:
-			'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-		lastSeen: '3h ago',
-		lastSeenDateTime: '2023-01-23T13:23Z',
-	},
-	{
-		name: 'Tom Cook',
-		email: 'tom.cook@example.com',
-		role: 'Director of Product',
-		imageUrl:
-			'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-		lastSeen: null,
-	},
-]
+
+import { useAuthContext } from "../../contexts/AuthContext";
+
+import FavoriteItem from "./favorite-item/FavoriteItem";
+import { useGetAllFavorites } from "../../hooks/useFavorite";
 
 export default function Favotites() {
+	const { email } = useAuthContext();
+	const [ favorites, setFavourite ] = useGetAllFavorites(email)
+
 	return (
 		<ul role="list" className="divide-y divide-gray-100 mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-3xl lg:px-8">
-			{people.map((person) => (
-				<li key={person.email} className="flex justify-between gap-x-6 py-5">
-					<div className="flex min-w-0 gap-x-4">
-						<img alt="" src={person.imageUrl} className="h-12 w-12 flex-none rounded-full bg-gray-50" />
-						<div className="min-w-0 flex-auto">
-							<p className="text-sm font-semibold leading-6 text-gray-900">{person.name}</p>
-							<p className="mt-1 truncate text-xs leading-5 text-gray-500">{person.email}</p>
-						</div>
-					</div>
-					<div className="hidden shrink-0 sm:flex sm:flex-row sm:items-end">
-						<button
-							type="button"
-							data-autofocus
-							className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
-						>
-							Details
-						</button>
-						<button
-							type="button"
-							className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
-						>
-							Remove
-						</button>
-					</div>
-				</li>
-			))}
+			{favorites.map(favorite => <FavoriteItem key={favorite._id} {...favorite} />)}
 		</ul>
 	)
 }
