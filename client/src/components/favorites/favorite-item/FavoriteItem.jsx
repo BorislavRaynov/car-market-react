494d91c8c65ad6401227d6c85e2123fd68b99dac
BorislavRaynov <b_raynov@abv.bg>
2024-08-01 18:44:20 +0300

    FavoriteItem component added.

---
+++ client/src/components/favorites/favorite-item/FavoriteItem.jsx
@@ -0,0 +1,52 @@
+import { useNavigate } from "react-router-dom";
+
+import * as request from '../../../api/requester'
+
+export default function FavoriteItem({ 
+    _id,
+    car,
+    changeState
+ }) {
+    const navigate = useNavigate()
+
+    const detailsClickHandler = (e) => {
+        e.preventDefault()
+        
+        navigate(`/cars/${car._id}/details`)
+    }
+
+    const deleteClickHandler = (e) => {
+        e.preventDefault()
+
+        request.del(`http://localhost:3030/data/favorites/${_id}`)
+    }
+
+    return (
+        <li className="flex justify-between gap-x-6 py-5">
+            <div className="flex min-w-0 gap-x-4">
+                <img alt="" src={car.img1} className="h-12 w-12 flex-none rounded-full bg-gray-50" />
+                <div className="min-w-0 flex-auto">
+                    <p className="text-sm font-semibold leading-6 text-gray-900">{car.make} {car.model}</p>
+                    <p className="mt-1 truncate text-xs leading-5 text-gray-500">{`${car.price} $`}</p>
+                </div>
+            </div>
+            <div className="hidden shrink-0 sm:flex sm:flex-row sm:items-end">
+                <button
+                    className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                    type="button"
+                    data-autofocus
+                    onClick={detailsClickHandler}
+                >
+                    Details
+                </button>
+                <button
+                    className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
+                    type="button"
+                    onClick={deleteClickHandler}
+                >
+                    Remove
+                </button>
+            </div>
+        </li>
+    );
+}
